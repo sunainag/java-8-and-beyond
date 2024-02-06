a43881a78280f9d9d995b16c636200d39ae6ad1d
sunainag <sunaina89.goyal@gmail.com>
2024-02-06 15:43:59 +0530

    lambda-expr-functional-interface

--- src/main/java/com/lambda/CalculatorImpl.java
+++ src/main/java/com/lambda/CalculatorImpl.java
@@ -3,14 +3,19 @@ package com.lambda;
 interface Calculator{
     void switchOn();
 }
-public class CalculatorImpl implements Calculator{
-    @Override
-    public void switchOn() {
-        System.out.println("traditional approach");
-    }
+public class CalculatorImpl{
+//    @Override
+//    public void switchOn() {
+//        System.out.println("traditional approach");
+//    }
 
     public static void main(String[] args) {
-        Calculator calculator = new CalculatorImpl();
+        //lambda expr syntax:
+        //parameter     expr        method body
+        //()            ->          {body}
+        //1. No need of method name/prefix, as this is anynonymous fn
+        //2. Assign it to interface
+        Calculator calculator = ()->System.out.println("Lambda expr");
         calculator.switchOn();
     }
 }
